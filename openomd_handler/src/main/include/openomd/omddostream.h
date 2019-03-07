d2014de5cf6316ae9611a5ffc8225c57eac4942b
raymondcheng <ray@raycray.com>
2019-03-07 13:44:33 +0800

    Update omdd outstream

--- openomd_handler/src/main/include/openomd/omddostream.h
+++ openomd_handler/src/main/include/openomd/omddostream.h
@@ -58,7 +58,7 @@ inline std::ostream& operator<<(std::ostream& os, CalculatedOpeningPrice const& 
 inline std::ostream& operator<<(std::ostream& os, ClassDefinition const& m)
 {
     os << "cld cty=" << (int16_t)m.country() << " mkt=" << (int16_t)m.market() << " ig=" << (int16_t)m.instrumentGroup() << " m=" << (int16_t)m.modifier() << " cc=" << m.commodityCode() << " pqf=" << m.priceQuotationFactor() << " cs=" << m.contractSize() << " dsp=" << m.decimalInStrikePrice() << " dcs=" << m.decimalInContractSize()
-            << " dp=" << m.decimalInPremium() << " rt=" << m.rankingType() << " tb=" << m.tradable() << " pu=" << m.premiumUnit4Price() << " bc=" << m.baseCurrency() << " icid=" << m.instrumentClassID() << " icn=" << m.instrumentClassName() << " if=" << m.isFractions() << " sc=" << m.settlementCurrencyID() << " et=" << m.effectiveTomorrow() << " tss=" << m.tickStepSize();
+            << " dp=" << m.decimalInPremium() << " rt=" << m.rankingType() << " tb=" << m.tradable() << " pu=" << m.premiumUnit4Price() << " bc=" << m.getBaseCurrencyAsString() << " icid=" << m.instrumentClassID() << " icn=" << m.instrumentClassName() << " if=" << m.isFractions() << " sc=" << m.settlementCurrencyID() << " et=" << m.effectiveTomorrow() << " tss=" << m.tickStepSize();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, CombinationDefinition const& m)
@@ -68,117 +68,118 @@ inline std::ostream& operator<<(std::ostream& os, CombinationDefinition const& m
 }
 inline std::ostream& operator<<(std::ostream& os, CommodityDefinition const& m)
 {
-    os << "cmd";
+    os << "cmd cc=" << m.commodityCode() << " dup=" << m.decimalInUnderlyingPrice() << " isin=" << m.getIsinCodeAsString() << " bc=" << m.getBaseCurrencyAsString() << " upu=" << (int16_t)m.underlyingPriceUnit() << " cn=" << m.getCommodityNameAsString() << " nv=" << m.nominalValue() << " uc=" << m.getUnderlyingCodeAsString() << " ut=" << (int16_t)m.underlyingType() << "et=" << (uint16_t)m.effectiveTomorrow() << " cid=" << m.getCommodityIDAsString();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, CommodityStatus const& m)
 {
-    os <<"cms";
+    os <<"cms cc=" << m.commodityCode() << " ss=" << m.suspended();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, DeleteOrder const& m)
 {
-    os <<"do";
+    os <<"do ob=" << m.orderbookID() << " oid=" << m.orderID() << " s=" << (int16_t)m.side();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, DisasterRecoverySignal const& m)
 {
-    os <<"drs";
+    os <<"drs drs=" << m.drStatus();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, EstimatedAverageSettlementPrice const& m)
 {
-    os <<"eas";
+    os <<"eas et=" << m.easType() << " ic=" << m.getInstrumentCodeAsString() << " eas=" << m.eas();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, ImpliedVolatility const& m)
 {
-    os <<"iv";
+    os <<"iv ob=" << m.orderbookID() << " iv=" << m.impliedVolatility();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, LogonResponse const& m)
 {
-    os <<"lrs";
+    os <<"lrs ss=" << m.sessionStatus();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, MarketAlert const& m)
 {
-    os <<"mka";
+    os <<"mka aid=" << m.alertID() << " s=" << m.source() << " header=" << m.getHeaderAsString();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, MarketStatus const& m)
 {
-    os <<"mks";
+    os <<"mks sl=" << m.stateLevel() << " mkt=" << (uint16_t)m.market() << " inst=" << (uint16_t)m.instrument() << " ob=" << m.orderbookID() << " cc=" << m.commodityCode() << " asd=" << m.actualStartDate() << " ast=" << m.actualStartTime() << " psd=" << m.plannedStartDate() << " pst=" << m.plannedStartTime() << " ssc=" << m.secondsToStateChange() << " s=" << m.state() << " p=" << (uint16_t)m.priority();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, ModifyOrder const& m)
 {
-    os <<"mo";
+    os <<"mo ob=" << m.orderbookID() << " oid=" << m.orderID() << " p=" << m.price() << " qty=" << m.quantity() << " s=" << (uint16_t)m.side() << " ot=" << m.orderType() << " obp=" << m.orderBookPosition();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, OpenInterest const& m)
 {
-    os <<"oi";
+    os <<"oi di=" << m.dayIndicator() << " ob=" << m.orderbookID() << " st=" << m.settlement() << " dc=" << m.dealCount() << " gOI=" << m.grossOI() << " nOI=" << m.netOI() << " to=" << m.turnover();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, OrderbookClear const& m)
 {
-    os <<"obc";
+    os <<"obc ob=" << m.orderbookID();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, QuoteRequest const& m)
 {
-    os <<"qr";
+    os <<"qr ob=" << m.orderbookID() << " lot=" << m.numberOfLots() << " ba=" << (uint16_t)m.bidAskFlag();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, RefreshComplete const& m)
 {
-    os <<"rfc";
+    os <<"rfc seq=" << m.lastSeqNum();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, RetransmissionResponse const& m)
 {
-    os <<"rrs";
+    os <<"rrs cid=" << m.channelID() << " rs=" << m.retransStatus() << " bsn=" << m.beginSeqNum() << " esn=" << m.endSeqNum();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, SequenceReset const& m)
 {
-    os <<"ser";
+    os <<"ser nsn=" << m.newSeqNo();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, SeriesDefinitionBase const& m)
 {
-    os <<"sd";
+    os <<"sd ob=" << m.orderbookID() << " sym=" << m.symbol() << " fp=" << (uint16_t)m.financialProduct() << " ndp=" << m.numberOfDecimalsPrice() << " nl=" << m.numberOfLegs() << " sp=" << m.strikePrice() << " ed=" << m.expirationDate() << " pc=" << (uint16_t)m.putOrCall();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, SeriesDefinitionExtended const& m)
 {
-    os <<"sde";
+    os <<"sde ob=" << m.orderbookID() << " sym=" << m.getSymbolAsString() << " c=" << (uint16_t)m.country() << " mkt=" << (uint16_t)m.market() << " ig=" << (uint16_t)m.instrumentGroup() << " m=" << (uint16_t)m.modifier() << " cc=" << m.commodityCode() << " ed=" << m.expirationDate() << " sp=" << m.strikePrice() << " cs=" << m.contractSize() 
+        << " ic=" << m.getIsinCodeAsString() << " ss=" << (uint16_t)m.seriesStatus() << " et=" << (uint16_t)m.effectiveTomorrow() << " pqf=" << m.priceQuotationFactor() << " exp=" << m.getEffectiveExpDateAsString() << " dt=" << m.dateTimeLastTrading();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, SeriesStatistics const& m)
 {
-    os <<"sst";
+    os <<"sst ob=" << m.orderbookID() << " ss=" << (uint16_t)m.session() << " op=" << m.open() << " h=" << m.high() << " l=" << m.low() << " trv=" << m.tradeReportVolume() << " dc=" << m.dealCount() << " p=" << m.price() << " to="<< m.turnover();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, SeriesStatus const& m)
 {
-    os <<"sss";
+    os <<"sss ob=" << m.orderbookID() << " ss=" << m.suspended();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, Trade const& m)
 {
-    os <<"trd";
+    os << "trd ob=" << m.orderbookID() << " oid=" << m.orderID() << " p=" << m.price() << " tid=" << m.tradeID() << " cgid=" << m.comboGroupID() << " sd=" << m.side() << " dt=" << m.dealType() << " tc=" << m.tradeCondition() << " di=" << m.dealInfo() << " qty=" << m.quantity() << " tt=" << m.tradeTime();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, TradeAmendment const& m)
 {
-    os <<"tam";
+    os <<"tam tid=" << m.tradeID() << " cgid=" << m.comboGroupID() << " p=" << m.price() << " qty=" << m.quantity() << " tt=" << m.tradeTime() << " ts=" << m.tradeState();
     return os;
 }
 inline std::ostream& operator<<(std::ostream& os, TradeStatistics const& m)
 {
-    os <<"tst";
+    os <<"tst ob=" << m.orderbookID() << " p=" << m.price() << " ds=" << (uint16_t)m.dealSource() << " ss=" << (uint16_t)m.session() << " agq=" << m.aggregateQuantity() << " o=" << m.open() << " h=" << m.high() << " l=" << m.low() << " trv=" << m.tradeReportVolume() << " dc=" << m.dealCount() << " to=" << m.turnover();
     return os;
 }
 }
